18b25463dc947e50c5efa450e931233520c82f88
Jose Torres <vidojam@gmail.com>
2023-05-09 14:39:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+// Console.WriteLine("Hello, World!");
+
+int age = 26;
+string name = "Damian";
+bool isHere = true;
+double salary = 22000.01;
+
+
+// Console.WriteLine("Your age is " + age);
+Console.WriteLine($" hi {name} Your age is {age} and is {isHere}");
+Console.WriteLine($"And you make ${salary} a year");
